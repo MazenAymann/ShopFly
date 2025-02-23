a9fffc5db3295e791e466ddaa4dfc4360ffdcd0f
Mazen.Ayman <mazenaymanmo22@gmail.com>
2025-02-23 13:34:17 +0200

    feat(Flutter): Setting up firebase for Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  //Adding configs for Android version, else don't add for ios
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+            apiKey: "AIzaSyDGrgrUkIua5A6LMLUnbqlqcUYn6hL4YJk",
+            appId: "1:1012437149503:android:a8ff52ed5cc23ae375d58a",
+            messagingSenderId: "1012437149503",
+            projectId: "shopfly-store",
+          ),
+        )
+      : await Firebase.initializeApp();
+
   runApp(const MainApp());
 }
 
